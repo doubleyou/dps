a10f9bdfee888364711e00a94f1e20058b99ccda
Max Lapshin <max@maxidoors.ru>
2012-10-05 22:34:09 +0400

    dps_channels_sup fix

--- src/dps_channels_sup.erl
+++ src/dps_channels_sup.erl
@@ -15,14 +15,14 @@ start_link() ->
 
 start_channel(Name) ->
   ChannelSpec = {Name, {dps_channel, start_link, [Name]}, transient, 5000, worker, [dps_channel]},
-  case supervisor:start_child(dps_channels, ChannelSpec) of
+  case supervisor:start_child(dps_channels_sup, ChannelSpec) of
     {ok, Pid} -> {ok, Pid};
     {error, {already_started, Pid}} -> {ok, Pid}
   end.
 
 stop_channel(Name) ->
-  supervisor:terminate_child(dps_channels, Name),
-  supervisor:delete_child(dps_channels, Name).
+  supervisor:terminate_child(dps_channels_sup, Name),
+  supervisor:delete_child(dps_channels_sup, Name).
 
 
 
@@ -30,17 +30,5 @@ stop_channel(Name) ->
 %% supervisor callbacks
 %%
 
-init([channels]) ->
-  {ok, {{one_for_one, 5, 10}, []}};
-
-
 init([]) ->
-
-    ChannelsSup = {dps_channels, 
-      {supervisor, start_link, [{local, dps_channels}, ?MODULE, [channels]]},
-      permanent,
-      infinity,
-      supervisor,
-      []
-    },
-    {ok, { {one_for_one, 5, 10}, [ChannelsSup]} }.
+    {ok, { {one_for_one, 5, 10}, []} }.
