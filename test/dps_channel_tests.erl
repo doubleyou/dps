32a4df010dc0987668b3e630a83aa00d1472fd26
Max Lapshin <max@maxidoors.ru>
2012-10-05 20:31:30 +0400

    channel tests

--- test/dps_channel_tests.erl
+++ test/dps_channel_tests.erl
@@ -6,30 +6,41 @@
 
 
 dps_channel_test_() ->
+  TestFunctions = [F || {F,0} <- ?MODULE:module_info(exports),
+                            lists:prefix("test_", atom_to_list(F))],
   {foreach,
-  fun setup_dps_channel/0,
-  fun teardown_dps_channel/1,
-  [
-    fun simple_pub_sup/0
-  ]}.
+    fun setup/0,
+    fun teardown/1,
+    [{atom_to_list(F), fun ?MODULE:F/0} || F <- TestFunctions]
+  }.
 
 
-setup_dps_channel() ->
-  Modules = [dps_channel_sup, dps_channel],
+setup() ->
+  Modules = [dps_channel_sup, dps_channel, dps_util],
   meck:new(Modules, [{passthrough, true}]),
-  {ok, Pid} = dps_channel:start_link(test_chan),
-  unlink(Pid),
-  {Modules, Pid}.
+  gen_event:delete_handler(error_logger, error_logger_tty_h, []),
+  application:start(dps),
+  {Modules}.
 
 
-teardown_dps_channel({Modules, Pid}) ->
+teardown({Modules}) ->
   meck:unload(Modules),
-  erlang:exit(Pid, shutdown),
+  application:stop(dps),
   ok.
 
 
 
-simple_pub_sup() ->
+test_channel_publish() ->
+  dps_channels_manager:create(test_channel),
+  meck:expect(dps_util, ts, fun() -> 123456 end),
+  ?assertEqual(123456, dps_channel:publish(test_channel, message)),
+  ok.
+
+test_channel_get_all_messages() ->
+  dps_channels_manager:create(test_channel),
+  meck:expect(dps_util, ts, fun() -> 123456 end),
+  dps_channel:publish(test_channel, message),
+  ?assertEqual({ok, 123456, [message]}, dps_channel:messages(test_channel, undefined)),
   ok.
 
 
