102af3f5a6ee56151011b3a85b59c32d1f45817d
Max Lapshin <max@maxidoors.ru>
2012-10-06 17:08:06 +0400

    add first benchmarkings

---
+++ src/dps_benchmark.erl
@@ -0,0 +1,41 @@
+-module(dps_benchmark).
+-compile(export_all).
+
+
+-define(COUNT, 32).
+
+run1() ->
+  application:start(dps),
+
+  dps:new(channel),
+  spawn(fun() ->
+    timer:send_interval(1000, dump),
+    put(now, erlang:now()),
+    run1_sender(channel, 1)
+  end),
+
+  % Receivers = [spawn(fun() ->
+  %   Chan = I div 4,
+  %   Channels = [Chan, (Chan+1) rem ?COUNT, (Chan+2) rem ?COUNT, (Chan + 3) rem ?COUNT],
+  %   run1_receiver(Channels, 1, 0)
+  % end) || I <- lists:seq(0,?COUNT*4 - 1)],
+  ok.
+
+
+
+
+run1_sender(Chan, Number) ->
+  receive
+    dump ->
+      Delta = timer:now_diff(erlang:now(), get(now)) div 1000000,
+      if Delta > 0 ->
+        io:format("Send: ~B msg/s~n", [Number div Delta]);
+      true -> ok end
+  after 0 -> ok end,
+  dps:publish(Chan, Number),
+  run1_sender(Chan, Number + 1).
+
+
+% run1_receiver(Channels, Number, TS) ->
+%   {ok, LastTS, Messages} = dps:multi_fetch(Channels, TS),
+%   run1_receiver(Channels, Number + length(Messages), LastTS).
