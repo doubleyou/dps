fac3b6b98141b07d19bf10b5566be1f4f93042a6
Max Lapshin <max@maxidoors.ru>
2012-10-07 12:10:13 +0400

    now it is multichannel benchmark

--- src/dps_benchmark.erl
+++ src/dps_benchmark.erl
@@ -5,17 +5,22 @@
 -define(COUNT, 32).
 
 run1() ->
+  run1(3).
+
+run1(ChannelCount) ->
   application:start(dps),
 
-  dps:new(channel),
-  spawn(fun() ->
+  SendCollector = spawn(fun() ->
     timer:send_interval(1000, dump),
     put(now, erlang:now()),
-    run1_sender(channel, 1)
+    send_collector(0)
   end),
+  erlang:register(send_collector, SendCollector),
+  [proc_lib:start_link(?MODULE, start_sender, [{channel, I}]) || I <- lists:seq(1,ChannelCount)],
 
   spawn(fun() ->
-    run1_receiver([channel], 0)
+    Channels = [{channel,I} || I <- lists:seq(1,ChannelCount)],
+    run1_receiver(Channels, 0, dict:new())
   end),
   % Receivers = [spawn(fun() ->
   %   Chan = I div 4,
@@ -25,23 +30,45 @@ run1() ->
   ok.
 
 
-run1_receiver(Channels, TS) ->
-  {ok, LastTS, Messages} = dps:multi_fetch(Channels, TS),
-  if
-    length(Messages) > 10 -> io:format("Messages: ~p~n", [length(Messages)]);
-  true -> ok end,
-  run1_receiver(Channels, LastTS).
-
-run1_sender(Chan, Number) ->
+send_collector(Total) ->
   receive
+    {messages, _Channel, Count} -> send_collector(Total + Count);
     dump ->
       Delta = timer:now_diff(erlang:now(), get(now)) div 1000000,
       if Delta > 0 ->
-        io:format("Send: ~B msg/s~n", [Number div Delta]);
-      true -> ok end
-  after 0 -> ok end,
-  dps:publish(Chan, Number),
-  run1_sender(Chan, Number + 1).
+        io:format("Send: ~B msg/s~n", [Total div Delta]);
+      true -> ok end,
+      send_collector(Total)
+  end.
+
+
+start_sender(Chan) ->
+  timer:send_interval(1000, dump),
+  dps:new(Chan),
+  proc_lib:init_ack(self()),
+  run1_sender(Chan, 1, 1).
+
+run1_sender(Chan, Number, Count) ->
+  Count1 = receive
+    dump -> send_collector ! {messages, Chan, Count}, 0
+  after 0 -> Count + 1 end,
+  dps:publish(Chan, {Chan, Number}),
+  run1_sender(Chan, Number + 1, Count1).
+
+
+
+run1_receiver(Channels, TS, Stats) ->
+  {ok, LastTS, Messages} = dps:multi_fetch(Channels, TS),
+  {Stats1, Dropped} = collect_receive_stats(Stats, Messages),
+  % if
+  %   length(Messages) > 10 -> io:format("Messages: ~p~n", [length(Messages)]);
+  % true -> ok end,
+  if Dropped > 0 -> io:format("Receiver dropped ~p msg~n", [Dropped]);
+    true -> ok end,
+  run1_receiver(Channels, LastTS, Stats1).
+
+collect_receive_stats(Stats, Messages) ->
+  {Stats, 0}.
 
 
 % run1_receiver(Channels, Number, TS) ->
