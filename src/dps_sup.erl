c4abcca62a6a4e86aca794575295f73767763336
Max Lapshin <max@maxidoors.ru>
2012-10-07 18:18:30 +0400

    dps_sup now starts replicator and channel

--- src/dps_sup.erl
+++ src/dps_sup.erl
@@ -16,11 +16,13 @@ start_link() ->
 
 
 start_channel(Name) ->
-  ChannelSpec = {Name, {dps_channel, start_link, [Name]}, transient, 5000, worker, [dps_channel]},
-  case supervisor:start_child(dps_channels_sup, ChannelSpec) of
-    {ok, Pid} -> {ok, Pid};
-    {error, {already_started, Pid}} -> {ok, Pid}
-  end.
+  ChannelSpec = {Name, {supervisor, start_link, [?MODULE, {channel,Name}]}, permanent, infinity, supervisor, []},
+  {ok, Supervisor} = case supervisor:start_child(dps_channels_sup, ChannelSpec) of
+    {ok, SupPid} -> {ok, SupPid};
+    {error, {already_started, SupPid}} -> {ok, SupPid}
+  end,
+  {channel, Pid, _, _} = lists:keyfind(channel, 1, supervisor:which_children(Supervisor)),
+  {ok, Pid}.
 
 stop_channel(Name) ->
   supervisor:terminate_child(dps_channels_sup, Name),
@@ -30,6 +32,12 @@ stop_channel(Name) ->
 %%
 %% supervisor callbacks
 %%
+
+init({channel, Name}) ->
+  {ok, {{one_for_all, 5, 10}, [
+    {channel, {dps_channel, start_link, [Name]}, permanent, 5000, worker, [dps_channel]},
+    {replicator, {dps_channel_replicator, start_link, [Name]}, permanent, 5000, worker, [dps_channel_replicator]}
+  ]}};
 
 init([channels]) ->
   {ok, { {one_for_one, 5, 10}, []} };
