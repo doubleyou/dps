0b5ae927bd5646c7dab30183f3866f10f4bef71c
Max Lapshin <max@maxidoors.ru>
2012-10-15 00:17:11 +0400

    Bench is now more smoother

--- src/bench.erl
+++ src/bench.erl
@@ -5,11 +5,17 @@
 
 -define(OPTIONS, [
     {clients, 1000},
-    {channels, 10},
-    {channels_per_client, {1, 2}},
+    {channels, 40},
+    {channels_per_client, {1, 4}},
     {pub_interval, 1000},
     {hosts, ["localhost"]}
 ]).
+
+
+% ClientCount * ((MaxChannel + MinChannel)/2) / PubInterval = Messages/MSec
+% MessagesPerChannelPerSec = ClientCount * ((MaxChannel + MinChannel)/2)*1000 / (Channels*PubInterval)
+% OutRate = Clients*1000 / PubInterval
+% InRate = MessagesPerChannelPerSec*Clients
 
 -define(PORT, 9201).
 -define(AVG_SIZE, 36).
@@ -35,9 +41,6 @@ stats() ->
 
 start(Opts) ->
     Options = Opts ++ ?OPTIONS,
-    inets:start(),
-    inets:start(httpc, [{profile, push_httpc}]),
-    httpc:set_options([{max_sessions,10240},{max_keep_alive_length,1000}], push_httpc),
 
     Clients = proplists:get_value(clients, Options),
     {MinChannels,MaxChannels} = proplists:get_value(channels_per_client, Options),
@@ -48,7 +51,9 @@ start(Opts) ->
 
     ets:new(stats, [public, named_table, set]),
     ets:insert(stats, {publishes, 0}),
+    ets:insert(stats, {total_publishes, 0}),
     ets:insert(stats, {messages, 0}),
+    ets:insert(stats, {total_messages, 0}),
     ets:insert(stats, {start_at, erlang:now()}),
 
     proc_lib:spawn_link(fun stats_collector/0),
@@ -64,12 +69,12 @@ start(Opts) ->
         end
     end) || I <- lists:seq(1, Clients)],
 
-    % _Pollers = [proc_lib:spawn_link(fun() ->
-    %     timer:sleep(PubInterval*I div Clients),
-    %     PublishChannels = select_random(Channels, MinChannels, MaxChannels),
-    %     Host = lists:nth(I rem length(Hosts) + 1, Hosts),
-    %     start_poll(#state{channels = PublishChannels, host = Host, publish_interval = PubInterval})
-    % end) || I <- lists:seq(1, Clients div 40)],
+    _Pollers = [proc_lib:spawn_link(fun() ->
+        timer:sleep(PubInterval*I div Clients),
+        PublishChannels = select_random(Channels, MinChannels, MaxChannels),
+        Host = lists:nth(I rem length(Hosts) + 1, Hosts),
+        start_poll(#state{channels = PublishChannels, host = Host, publish_interval = PubInterval})
+    end) || I <- lists:seq(1, Clients div 40)],
     ok.
 
 select_random(Channels,Min,Max) ->
@@ -90,10 +95,12 @@ make_channel(I) ->
 stats_collector() ->
     timer:sleep(1000),
     [{publishes, PublishCount}] = ets:lookup(stats, publishes),
+    [{_, TotalPublishCount}] = ets:lookup(stats, total_publishes),
     [{messages, MessageCount}] = ets:lookup(stats, messages),
+    [{_, TotalMessageCount}] = ets:lookup(stats, total_messages),
     [{start_at, StartAt}] = ets:lookup(stats, start_at),
     _Delta = timer:now_diff(erlang:now(), StartAt) div 1000,
-    io:format("~B publish, ~B receive~n", [PublishCount, MessageCount]),
+    io:format("~6B publish (~7B), ~6B receive (~7B)~n", [PublishCount, TotalPublishCount, MessageCount, TotalMessageCount]),
     ets:insert(stats, {publishes, 0}),
     ets:insert(stats, {messages, 0}),
     ets:insert(stats, {start_at, erlang:now()}),
@@ -120,14 +127,20 @@ push(C1, URL, [Chan|Channels], Interval) ->
 
     % httpc:request(post, {URL, [], "text/plain", Chan ++ "|y u no love node.js?"}, [{timeout, 3000}], [], push_httpc),
     ets:update_counter(stats, publishes, 1),
+    ets:update_counter(stats, total_publishes, 1),
     timer:sleep(Interval),
     push(C4, URL, Channels ++ [Chan], Interval).
 
 
-start_poll(#state{channels = Channels, host = Host}) ->
+start_poll(#state{channels = Channels, host = Host} = State) ->
     {ok, C} = cowboy_client:init([]),
     URL = iolist_to_binary(io_lib:format("http://~s:~B/poll?channels=~s&ts=", [Host, ?PORT, string:join(Channels, ",")])),
-    poll(C, URL, 0).
+    try poll(C, URL, 0)
+    catch
+        error:{badmatch,{error,closed}} ->
+            start_poll(State)
+    end.
+
 
 poll(C1, URL, LastTS) ->
     T = list_to_binary(integer_to_list(LastTS)),
@@ -138,11 +151,12 @@ poll(C1, URL, LastTS) ->
     TS = case binary:split(Body, <<",">>) of
         [T1, Body1] ->
             ets:update_counter(stats, messages, size(Body1) div ?AVG_SIZE),
+            ets:update_counter(stats, total_messages, size(Body1) div ?AVG_SIZE),
             cowboy_http:digits(T1);
         [T1] ->
             cowboy_http:digits(T1)
     end,
-    timer:sleep(3000 + random:uniform(3000)),
+    timer:sleep(100),
     poll(C4, URL, TS).
 
 
