a7a7518b8e19aa7fb5998ff894646fc093e0bb71
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-25 19:09:32 +0800

    Delete src directory

--- src/dps_aws.erl
+++
@@ -1,8 +0,0 @@
--module(dps_aws).
-
--export([start/0]).
-
-start() ->
-    {ok, Nodes} = file:consult("priv/aws.cfg"),
-    lists:map(fun net_adm:ping/1, Nodes),
-    dps_example:start_everything().
--- src/dps_util.erl
+++
@@ -1,7 +0,0 @@
--module(dps_util).
-
--export([ts/0]).
-
-ts() ->
-    {Megasecs, Secs, Microsecs} = now(),
-    Microsecs + 1000000 * (Secs + 1000000 * Megasecs).
